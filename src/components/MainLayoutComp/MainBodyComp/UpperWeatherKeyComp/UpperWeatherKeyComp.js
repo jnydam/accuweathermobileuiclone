6d55f5b4ffe5b699cb207f533fe360b6c2b2d3bd
John Nydam <jnydam@me.com>
2022-07-06 21:28:59 -0400

    Added the needed assets to the project

---
+++ src/components/MainLayoutComp/MainBodyComp/UpperWeatherKeyComp/UpperWeatherKeyComp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpperWeatherKeyComp = (props) => {
+
+    return (<div className={styles.dynamicSunDisplayCompContainer}>
+        <span>This is the upper weather key comp container</span>
+    </div>
+    )
+};
+
+export default UpperWeatherKeyComp;
