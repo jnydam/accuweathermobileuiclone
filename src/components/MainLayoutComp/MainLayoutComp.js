11018bffc4117bacd9dc1df4b8e511a228816d18
John Nydam <jnydam@me.com>
2022-07-18 07:19:42 -0400

    Added more work to the header component

--- src/components/MainLayoutComp/MainLayoutComp.js
+++ src/components/MainLayoutComp/MainLayoutComp.js
@@ -3,12 +3,14 @@ import MainBodyComp from './MainBodyComp/MainBodyComp';
 import UpperNavBar from './UpperNavBar/UpperNavBar';
 
 import styles from './MainLayoutComp.module.css';
+import LowerTabComp from './LowerTabComp/LowerTabComp';
 
 const MainLayoutComp = (props) => {
 
     return (<div className={styles.mainLayoutCompContainer}>
         <UpperNavBar></UpperNavBar>
         <MainBodyComp></MainBodyComp>
+        <LowerTabComp></LowerTabComp>
     </div>)
 }
 
--- src/components/MainLayoutComp/UpperNavBar/UpperNavBar.js
+++ src/components/MainLayoutComp/UpperNavBar/UpperNavBar.js
@@ -2,10 +2,29 @@ import React from 'react';
 
 import styles from './UpperNavBar.module.css';
 
+import sideBarGraphic from '../../../assets/sidebaricon.png';
+import downArrowGraphic from '../../../assets/downarrow.png';
+
 const UpperNavBar = (props) => {
 
     return (<div className={styles.upperNavBarContainer}>
-        <span>This is the upper nav bar</span>
+        <div style={{justifyContent: 'flex-start'}} className={styles.tripleElementContainer}>
+            <img style={{height: '3rem'}} src={sideBarGraphic}>
+            </img>
+        </div>
+        <div className={styles.tripleElementContainer}>
+            <div className={styles.locationHeaderContainer}>
+                <span style={{color: 'white', fontWeight: 'bold'}}>Manchester, NH</span>
+                <img 
+                    style={{height: '1rem'}}
+                    src={downArrowGraphic}></img>
+            </div>
+        </div>
+        <div className={styles.tripleElementContainer}>
+            <button className={styles.premiumButtonStyle}> 
+                <span>GET PREMIUM+</span>
+            </button>
+        </div>
     </div>)
 }
 
