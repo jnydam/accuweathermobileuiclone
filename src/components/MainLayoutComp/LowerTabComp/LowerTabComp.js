13000feb748cf89a7795e5bbcb84108fc3cbcbe4
John Nydam <jnydam@me.com>
2022-07-19 17:53:40 -0400

    Fixed the lower tab styling

--- src/components/MainLayoutComp/LowerTabComp/LowerTabComp.js
+++ src/components/MainLayoutComp/LowerTabComp/LowerTabComp.js
@@ -2,10 +2,34 @@ import React from 'react';
 
 import styles from './LowerTabComp.module.css';
 
+import dailyTabGraphic from '../../../assets/dailytabicon.png';
+import todayTabGraphic from '../../../assets/todaytabicon.png';
+import hourlyTabGraphic from '../../../assets/hourlytabicon.png';
+import radarTabGraphic from '../../../assets/radartabicon.png';
+
 const LowerTabComp = (props) => {
 
     return (<div className={styles.lowerTabCompContainer}>
-        <span>This is the lower tab comp</span>
+        <div className={styles.tabDivider}>
+            <img className={styles.tabIconStyle} src={todayTabGraphic}>
+            </img>
+            <span className={styles.tabTextStyle}>Today</span>
+        </div>
+        <div className={styles.tabDivider}>
+             <img className={styles.tabIconStyle} src={hourlyTabGraphic}>
+            </img>
+            <span className={styles.tabTextStyle}>Hourly</span>
+        </div>
+        <div className={styles.tabDivider}>
+            <img className={styles.tabIconStyle} src={dailyTabGraphic}>
+            </img>
+            <span className={styles.tabTextStyle}>Daily</span>
+        </div>
+        <div className={styles.tabDivider}>
+            <img className={styles.tabIconStyle} src={radarTabGraphic}>
+            </img>
+            <span className={styles.tabTextStyle}>Radar</span>
+        </div>
     </div>)
 }
 
